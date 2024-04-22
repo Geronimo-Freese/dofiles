d3b0bb3651f3ebd7d4d713a484c958db6e649ea9
gnss19 <geronimo.freese@mni.thm.de>
2024-04-22 18:16:30 +0200

    Adding config for git
    Why: it annoyed me that i did not use commit templates
    
    How: By creating a commit template text file and changing git settings
    with "git config ..."

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Git
+mkdir ~/.config/git
+ln -s $(pwd)/git/commit_msg_template.txt ~/.config/git/commit_msg_template.txt
+git config --global commit.template ~/.config/git/commit_msg_template.txt
 
 # Create symlink for ~/.xsession
-ln -s $(pwd)/xsession/.xsession ~/.xsession
+# ln -s $(pwd)/xsession/.xsession ~/.xsession
