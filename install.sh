56a2cb7762d861db03164daa16d5f51862484f5d
Geronimo-Freese <geronimo.freese@gmail.com>
2024-04-22 21:43:20 +0200

    Trying some things for the installation -> maybe move to ansible

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# create directories
+# tree -dSfi | head -n -2 | tail -n +2 | xargs -I'{}' mkdir /tmp/'{}';
+
+# copy everything
+cp -r $(pwd)/ /tmp/
+
+
 # Git
 mkdir ~/.config/git
 ln -s $(pwd)/git/commit_msg_template.txt ~/.config/git/commit_msg_template.txt
